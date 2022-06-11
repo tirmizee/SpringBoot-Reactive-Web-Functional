601f2fb9797935b92d39ea8f81f544fddad89e30
Pratya Yeekhaday <pratya.yeekhaday@accenture.com>
2022-06-11 21:29:50 +0700

    init project

---
+++ src/main/java/com/tirmizee/SpringBootReactiveWebFunctional/SpringBootReactiveWebFunctionalApplication.java
@@ -0,0 +1,13 @@
+package com.tirmizee.SpringBootReactiveWebFunctional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactiveWebFunctionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactiveWebFunctionalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tirmizee/SpringBootReactiveWebFunctional/SpringBootReactiveWebFunctionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tirmizee.SpringBootReactiveWebFunctional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReactiveWebFunctionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
